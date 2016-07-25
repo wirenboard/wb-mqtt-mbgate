3be6589ea034fd164a8c0cfe08139930db366fd5
Nikita webconn Maslov <wkernelteam@gmail.com>
2016-07-25 16:52:45 +0300

    server proof-of-concept
    Signed-off-by: Nikita webconn Maslov <wkernelteam@gmail.com>

---
+++ server.py
@@ -0,0 +1,299 @@
+#!/usr/bin/env python
+
+# Modbus TCP to MQTT gateway proof-of-concept
+
+from mosquitto import Mosquitto
+from threading import Lock
+import json
+import argparse
+from Queue import Queue
+
+from pymodbus.server.sync import StartTcpServer
+from pymodbus.datastore import ModbusSparseDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+
+from type_convert import mqtt2modbus, modbus2mqtt
+
+
+def dev2topic(t):
+    parts = t.split("/")
+    return str("/devices/" + parts[0] + "/controls/" + parts[1])
+
+
+def topic2dev(t):
+    parts = t.split("/")
+    return str(parts[2] + "/" + parts[4])
+
+def singleton(class_):
+    instances = {}
+
+    def getInstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+
+    return getInstance
+
+
+class MQTTClientException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return repr(self.msg)
+
+
+@singleton
+class MQTTClient(Mosquitto):
+    def __init__(self, client_id="", clean_session=True, userdata=None):
+
+        Mosquitto.__init__(self, client_id, clean_session, userdata)
+
+        # init local callback list
+        self.DataBlocks = list()
+        self.DataQueue = Queue()
+        self.DataProcessing = False
+        self.on_message = self.__class__.ProcessOnMessage
+        self.on_connect = self.__class__.ProcessOnConnect
+        self.on_publish = self.__class__.ProcessOnPublish
+
+    def addModbusDataBlock(self, block, single=True):
+        if single:
+            self.DataBlocks.append(block)
+        elif type(block) is list:
+            for b in block:
+                self.DataBlocks.append(b)
+        else:
+            raise MQTTClientException("can't add this as datablock")
+
+    def QueueMessage(self, topic, msg, qos=0, retain=True):
+        if self.DataSending:
+            self.DataQueue.put((topic, msg, qos, retain))
+        else:
+            self.DataSending = True
+            self.publish(topic, msg, qos, retain)
+
+    # MQTT callbacks
+    def ProcessOnMessage(self, userdata, msg):
+        # find data block with this topic
+        print "Received message from " + msg.topic
+
+        for datablock in self.DataBlocks:
+            if datablock.CheckTopic(topic2dev(msg.topic)):
+                datablock.setValues(topic2dev(msg.topic), msg.payload)
+
+    def ProcessOnConnect(self, userdata, rc):
+        # Subscribe to all required topics
+        for datablock in self.DataBlocks:
+            for topic in datablock.RegTopicMap.keys():
+                # print "Subscribing on topic " + dev2topic(topic)
+                self.subscribe(dev2topic(topic))
+
+    def ProcessOnPublish(self, userdata, mid):
+        if self.DataQueue.empty():
+            self.DataProcessing = False
+        else:
+            # Push next message from queue to MQTT
+            topic, msg, qos, retain = self.DataQueue.get()
+            self.publish(topic, msg, qos, retain)
+
+
+class MQTTDataBlock(ModbusSparseDataBlock):
+
+    def __init__(self, reg_config):
+        self.Config = reg_config
+
+        # Create register maps with topics and addresses as keys
+        self.RegTopicMap = {}
+        self.RegAddrMap = {}
+
+        for item in self.Config:
+            self.RegTopicMap[item["topic"]] = item
+            self.RegAddrMap[item["address"]] = item
+
+        # Create Modbus-specified fields
+        self.address = 0
+        self.default_value = 0
+
+        # Mutex to manipulate data within different threads
+        self.DataLock = Lock()
+
+    # Check if topic is in this datablock
+    def CheckTopic(self, topic):
+        return topic in self.RegTopicMap
+
+    # Internal functions of PyModbus
+    def validate(self, address, count=1):
+        address -= 1
+        print "[MB] Validating values %d@%d" % (count, address)
+        while count > 0:
+            if address not in self.RegAddrMap:
+                return False
+
+            item_len = self.RegAddrMap[address]["size"]
+            if self.RegAddrMap[address]["format"] != "varchar":
+                item_len /= 2
+            count -= item_len
+            address += item_len
+
+        return count == 0
+
+    def getValues(self, address, count=1):
+        if isinstance(address, str):  # for MQTT topics
+            pass
+        else:  # for Modbus access
+            address -= 1
+            value_container = []
+            while count > 0:
+                conf = self.RegAddrMap[address]
+                value_container.extend(conf["mb_value"].values())
+                item_len = conf["size"]
+                if conf["format"] != "varchar":
+                    item_len /= 2
+                count -= item_len
+                address += item_len
+
+            print value_container
+            return value_container
+
+        return None
+
+    def setValues(self, address, values):
+        """
+        Address could be either number or string:
+         * number is for Modbus access
+         * string is for access from MQTT handler
+
+        In MQTT case, only one value is expected (str)
+        """
+
+        value_container = None
+
+        if isinstance(address, str):  # MQTT topic case
+            if not isinstance(values, str):
+                raise TypeError("Only one string value is expected")
+            # try to find specified value
+            value_container = self.RegTopicMap[address]
+
+            # convert value to appropriate type
+            values = mqtt2modbus(values, value_container)
+            with self.DataLock:
+                value_container["mb_value"] = values
+        else:  # Modbus topic case
+            address -= 1  # address space should start from 0
+            count = len(values)
+            print "Got values for address " + str(address)
+            print values
+
+            offset = 0
+
+            while count > 0:
+                value_container = self.RegAddrMap[address]
+
+                item_len = value_container["size"]
+                if value_container["format"] != "varchar":
+                    item_len /= 2
+
+                cur_value = values[offset:offset + item_len]
+
+                with self.DataLock:
+                    value_container["mb_value"] = cur_value
+
+                topic = dev2topic(value_container["topic"])
+                payload = modbus2mqtt(cur_value, value_container)
+                MQTTClient().QueueMessage(topic, payload)
+
+                count -= item_len
+                address += item_len
+                offset += item_len
+
+
+
+        # parse values
+        return True
+
+
+class ContextBuilderException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return repr(self.msg)
+
+
+class ModbusContextBuilder:
+    def __init__(self, config_file_name):
+        with open(config_file_name) as data_file:
+            self.config = json.load(data_file)
+            self.units = dict()
+
+    def _buildUnits(self):
+        for block in ["discretes", "coils", "inputs", "holdings"]:
+            self._buildBlock(block)
+
+    def _buildBlock(self, block):
+        try:
+            configs = self.config["registers"][block]
+        except KeyError:
+            raise ContextBuilderException("no such data block: %s" % (block))
+
+        for item in configs:
+            unitId = item["unitId"]
+
+            if unitId not in self.units:
+                self.units[unitId] = dict()
+            if block not in self.units[unitId]:
+                self.units[unitId][block] = list()
+
+            if ("enabled" not in item or item["enabled"]) and item["size"] >= 0:
+                self.units[unitId][block].append(item)
+
+    def buildContext(self):
+        units = dict()
+
+        self._buildUnits()
+
+        for unitId in self.units.keys():
+            unitData = self.units[unitId]
+
+            # build data blocks
+            _di = MQTTDataBlock(unitData["discretes"])
+            _co = MQTTDataBlock(unitData["coils"])
+            _ir = MQTTDataBlock(unitData["inputs"])
+            _hr = MQTTDataBlock(unitData["holdings"])
+
+            MQTTClient().addModbusDataBlock([_di, _co, _ir, _hr], single=False)
+
+            units[unitId] = ModbusSlaveContext(
+                di = _di,
+                co = _co,
+                ir = _ir,
+                hr = _hr)
+
+        context = ModbusServerContext(slaves=units, single=False)
+
+        return context
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Modbus TCP to MQTT gateway")
+    parser.add_argument("-c", "--config", help="config file name", type=str,
+                default="/etc/wb-mqtt-mbgate.conf")
+    parser.add_argument("-s", "--server", help="MQTT server hostname", type=str,
+                default="localhost")
+    parser.add_argument("-p", "--port", help="MQTT server port", type=int,
+                default=1883)
+
+    args = parser.parse_args()
+
+    modbus_context = ModbusContextBuilder(args.config).buildContext()
+
+    mqtt_client = MQTTClient()
+    mqtt_client.connect(args.server, args.port)
+    mqtt_client.loop_start()
+
+
+    StartTcpServer(context=modbus_context)
+
+    while 1:
+        pass
