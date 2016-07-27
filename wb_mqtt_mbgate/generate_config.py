d56112e15772a4970d14a7455864720cc0ddc7e9
Nikita webconn Maslov <wkernelteam@gmail.com>
2016-07-27 15:46:30 +0300

    gen-config: order registers by topics
    Signed-off-by: Nikita webconn Maslov <wkernelteam@gmail.com>

--- wb_mqtt_mbgate/generate_config.py
+++ wb_mqtt_mbgate/generate_config.py
@@ -20,23 +20,35 @@ config_file = None
 
 
 class RegDiscr(object):
-    def __init__(self, topic, meta_type):
+    def __init__(self, topic, meta_type, enabled = False, address=-1, unitId=-1):
         self.topic = topic
-        self.address = -1
-        self.unitId = -1
+        self.address = address
+        self.unitId = unitId
         self.meta_type = meta_type
-        self.enabled = False
+        self.enabled = enabled
+
+    def getSize(self):
+        return 1
 
 
 class Register(RegDiscr):
-    def __init__(self, topic, meta_type, format="double", size=8, max=0, scale=1, byteswap=False, wordswap=False):
-        RegDiscr.__init__(self, topic, meta_type)
+    def __init__(self, topic, meta_type, enabled = False, address=-1, unitId=-1,
+    format="double", size=8, max=0, scale=1, byteswap=False, wordswap=False):
+        RegDiscr.__init__(self, topic, meta_type, enabled, address, unitId)
         self.format = format
         self.size = size
         self.max = max
         self.scale = scale
         self.byteswap = byteswap
         self.wordswap = wordswap
+
+    def getSize(self):
+        if self.size <= 0:
+            return 1
+        elif self.format == "varchar":
+            return self.size
+        else:
+            return self.size / 2
 
 
 class RegSpace:
@@ -46,12 +58,14 @@ class RegSpace:
         self.address = 0
 
     def append(self, value):
-        value.address = self.address % 65536
-        value.unitId = int(self.address / 65536) + 1
-        if isinstance(value, Register) and value.size > 0:
-            self.address += int(value.size) / 2
+        if value.address >= 0:  # handle this as old value
+            if self.address < value.address + value.getSize():
+                self.address = value.address + value.getSize()
         else:
-            self.address += 1
+            value.address = self.address % 65536
+            value.unitId = int(self.address / 65536) + 1
+            self.address += value.getSize()
+
         self.values.append(value)
 
     def getValues(self):
@@ -62,12 +76,12 @@ class RegSpace:
 
         return ret
 
-
 regs_discr = RegSpace("discretes")
 regs_coils = RegSpace("coils")
 regs_input = RegSpace("inputs")
 regs_holding = RegSpace("holdings")
 
+regs = { "discretes": regs_discr, "coils": regs_coils, "inputs": regs_input, "holdings": regs_holding }
 
 def get_dev_name(topic):
     lst = topic.split("/")
@@ -75,34 +89,61 @@ def get_dev_name(topic):
 
 
 def process_table(table):
-    global old_config
+    global old_config, regs
 
     result = dict()
-    # TODO: update feature - don't touch existing registers
+
+    # old config data
+    old_registers = []
+    old_topics = []
 
     result["address"] = "127.0.0.1"
     result["port"] = 502
     result["debug"] = False
 
     if old_config is not None:
+        print >>sys.stderr, "Old config is detected"
+        for c in old_config["registers"].keys():
+            for old_reg in old_config["registers"][c]:
+                old_reg["category"] = c
+                old_registers.append(old_reg)
+
         for f in ["address", "port", "debug"]:
             if f in old_config:
                 result[f] = old_config[f]
 
-    for topic in table.keys():
-        process_channel(table[topic], topic)
+    # fill configuration object according to old config
+    for reg in old_registers:
+        process_channel(reg, reg["topic"])
+        old_topics.append(reg["topic"])
+
+    for topic in sorted(table.keys()):
+        if topic not in old_topics:
+            process_channel(table[topic], topic)
+        else:
+            print >>sys.stderr, "Topic %s taken from old config" % (topic)
 
     result["registers"] = dict()
-    result["registers"]["discretes"] = regs_discr.getValues()
-    result["registers"]["coils"] = regs_coils.getValues()
-    result["registers"]["inputs"] = regs_input.getValues()
-    result["registers"]["holdings"] = regs_holding.getValues()
+
+    for cat in regs.keys():
+        result["registers"][cat] = regs[cat].getValues()
 
     return result
 
 
 def process_channel(obj, topic):
-    global regs_discr, regs_coils, regs_input, regs_holding
+    global regs
+
+    if "category" in obj:
+        cat = obj["category"]
+        del obj["category"]
+
+        if obj["meta_type"] in coil_formats:
+            regs[cat].append(RegDiscr(**obj))
+        else:
+            regs[cat].append(Register(**obj))
+
+        return
 
     text = False
 
@@ -111,33 +152,33 @@ def process_channel(obj, topic):
 
     if obj["meta_type"] in coil_formats:
         if "readonly" in obj and obj["readonly"]:
-            regs_discr.append(RegDiscr(topic, obj["meta_type"]))
+            regs["discretes"].append(RegDiscr(topic, obj["meta_type"]))
         else:
-            regs_coils.append(RegDiscr(topic, obj["meta_type"]))
+            regs["coils"].append(RegDiscr(topic, obj["meta_type"]))
     else:  # double format by default, but changeable by user
         if "readonly" in obj and obj["readonly"]:
             if text:
-                regs_input.append(Register(topic, obj["meta_type"], size=-1, format="varchar"))
+                regs["inputs"].append(Register(topic, obj["meta_type"], size=-1, format="varchar"))
             else:
-                regs_input.append(Register(topic, obj["meta_type"]))
+                regs["inputs"].append(Register(topic, obj["meta_type"]))
         else:
             if text:
-                regs_holding.append(Register(topic, obj["meta_type"], size=-1, format="varchar"))
+                regs["holdings"].append(Register(topic, obj["meta_type"], size=-1, format="varchar"))
             else:
-                regs_holding.append(Register(topic, obj["meta_type"]))
+                regs["holdings"].append(Register(topic, obj["meta_type"]))
 
 
 # apply MQTT retained hack to collect all MQTT retained values, then die
 def mqtt_on_message(arg0, arg1, arg2=None):
     msg = arg2 or arg1
 
+    print >>sys.stderr, msg.topic
+
     if msg.topic == retain_hack_topic:
         print >>config_file, json.dumps(process_table(table), indent=True)
         sys.exit(0)
 
     if msg.retain:
-        print >>sys.stderr, msg.topic
-
         if not topic_matches_sub("/devices/+/controls/name", msg.topic):
             if get_dev_name(msg.topic) not in table:
                 table[get_dev_name(msg.topic)] = {}
@@ -154,7 +195,7 @@ def mqtt_on_message(arg0, arg1, arg2=None):
 
 def main(args=None):
 
-    global old_config, config_file
+    global old_config, config_file, retain_hack_topic
 
     if args is None:
         parser = argparse.ArgumentParser(description="Config generator/updater for wb-mqtt-mbgate")
