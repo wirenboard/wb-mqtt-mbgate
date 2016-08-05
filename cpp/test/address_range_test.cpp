65abdf1deba4ccc9147c9cc780e0060d1a48c829
Nikita webconn Maslov <wkernelteam@gmail.com>
2016-08-05 16:43:00 +0300

    more tests for AddressRange
    Signed-off-by: Nikita webconn Maslov <wkernelteam@gmail.com>

--- cpp/test/address_range_test.cpp
+++ cpp/test/address_range_test.cpp
@@ -19,13 +19,113 @@ TEST_F(TAddressRangeTest, AddressTest)
     TestAddressRange test_range;
     TestAddressRange result_range;
 
-    // check mergeing
+    // check merging
     // --[--)-------- +
-    // -----[-----)-- ==
+    // -----[-----)-- =
     // --[--------)--
     test_range.insert(0, 10, 1);
     test_range.insert(10, 10, 1);
 
     result_range.insert(0, 20, 1);
-    ASSERT_EQ(test_range, result_range);
+    EXPECT_EQ(test_range, result_range);
+
+    test_range.clear();
+    result_range.clear();
+
+    // --[----)------- +
+    // ----[----)----- =
+    // --[------)-----
+    test_range.insert(-10, 20, 1);
+    test_range.insert(0, 20, 1);
+
+    result_range.insert(-10, 30, 1);
+    EXPECT_EQ(test_range, result_range);
+    
+    test_range.clear();
+    result_range.clear();
+
+    // --[---------)---- +
+    // ----[-----)------ =
+    // --[---------)----
+    test_range.insert(10, 30, 1);
+    test_range.insert(20, 10, 1);
+    
+    result_range.insert(10, 30, 1);
+    EXPECT_EQ(test_range, result_range);
+
+    test_range.clear();
+    result_range.clear();
+    
+    // ----[-----)------ +
+    // --[---------)---- =
+    // --[---------)----
+    test_range.insert(20, 10, 1);
+    test_range.insert(10, 30, 1);
+    
+    result_range.insert(10, 30, 1);
+    EXPECT_EQ(test_range, result_range);
+
+    test_range.clear();
+    result_range.clear();
+
+    // ---[---)----[---)--- +
+    // ----[----)---------- =
+    // ---[-----)--[---)---
+    test_range.insert(0, 10, 1);
+    test_range.insert(20, 10, 1);
+    test_range.insert(5, 10, 1);
+    
+    result_range.insert(0, 15, 1);
+    result_range.insert(20, 10, 1);
+    EXPECT_EQ(test_range, result_range);
+
+    test_range.clear();
+    result_range.clear();
+
+
+    // ---[---)----[---)--- +
+    // ----------[----)---- =
+    // ---[---)--[-----)---
+    test_range.insert(0, 10, 1);
+    test_range.insert(20, 10, 1);
+    test_range.insert(15, 10, 1);
+    
+    result_range.insert(0, 10, 1);
+    result_range.insert(15, 15, 1);
+    EXPECT_EQ(test_range, result_range);
+
+    test_range.clear();
+    result_range.clear();
+
+    // ---[---)----[---)--- +
+    // ------[--------)---- =
+    // ---[------------)---
+    test_range.insert(0, 10, 1);
+    test_range.insert(20, 10, 1);
+    test_range.insert(5, 20, 1);
+    
+    result_range.insert(0, 30, 1);
+    EXPECT_EQ(test_range, result_range);
+
+    test_range.clear();
+    result_range.clear();
+    
+    // ---[---)-[)--[---)--- +
+    // --[---------------)-- =
+    // --[---------------)--
+    test_range.insert(0, 10, 1);
+    test_range.insert(20, 10, 1);
+    test_range.insert(35, 10, 1);
+    test_range.insert(-10, 80, 1);
+    
+    result_range.insert(-10, 80, 1);
+    EXPECT_EQ(test_range, result_range);
+
+    test_range.clear();
+    result_range.clear();
+}
+
+TEST_F(TAddressRangeTest, FailureTest)
+{
+
 }
