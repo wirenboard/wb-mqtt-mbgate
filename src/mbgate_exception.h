d2d171562c9b79b8b0b14420a198fca41abe90e1
Nikita Maslov <nikita.maslov@wirenboard.com>
2024-08-20 17:18:25 +0500

    make service not fail on empty configuration

--- src/mbgate_exception.h
+++ src/mbgate_exception.h
@@ -8,3 +8,8 @@ class TConfigException: public std::runtime_error
 public:
     explicit TConfigException(const std::string& message);
 };
+
+class TEmptyConfigException: public std::runtime_error {
+public:
+    explicit TEmptyConfigException();
+};
